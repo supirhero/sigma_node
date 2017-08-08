e46cb049e068cb588b63e3055a5ec5bc5b30e435
dhani48 <dhaniforty8@gmail.com>
2017-08-09 00:01:10 +0700

    test

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,58 +4,60 @@ import axios from 'axios'
 import { Link, browserHistory } from 'react-router'
 import { routerMiddleware, push } from 'react-router-redux'
 
-import {Input} from './Components.jsx'
 import store from '../reducers/combineReducers.jsx'
 import {getData, login} from './actions.jsx'
+import {Input, Loader} from './Components.jsx'
 
 
 class Login extends Component {
     render(){
       return(
         <div className='grid wrap'>
-          <div className='unit whole'>
-            <div className='card shadow login' style={{padding:'0'}}>
-            <div className='unit two-fifths no-gutters' style={{height:'100%'}}>
-              <span style={{position:'relative'}}>
-                <div id='picture'>
-                  <img src={require('../img/ProuDS-logo-clean.png')} id='prouds-logo-big'/>
-                  <small>Project Management &</small>
-                  <small>Resource Delivery System</small>
+            <div className='unit whole'>
+              <div className='card shadow login' style={{padding:'0'}}>
+                <div className='unit two-fifths no-gutters' style={{height:'100%'}}>
+                  <span style={{position:'relative'}}>
+                    <div id='picture'>
+                      <img src={require('../img/ProuDS-logo-clean.png')} id='prouds-logo-big'/>
+                      <small>Project Management &</small>
+                      <small>Resource Delivery System</small>
+                    </div>
+                  </span>
                 </div>
-              </span>
-            </div>
-            <div className='unit three-fifths'>
-              <div className='margin'>
-                <large>SIGN IN</large>
-                <form onSubmit={
-                  e => {
+                <Loader id='login-loader'>
+                <div className='unit three-fifths'>
+                  <div className='margin'>
+                    <large>SIGN IN</large>
+                    <form onSubmit={
+                      e => {
+                        store.dispatch(login()).then(
+                          (res)=>{
 
-                    store.dispatch(login()).then(
-                      (res)=>{
+                          }
+                        )
 
+                        // console.log('store last', store.getState());
+
+                        e.preventDefault()
                       }
-                    )
+                    }>
 
-                    // console.log('store last', store.getState());
+                    <Input inputName='USERNAME' />
+                    <Input inputName='PASSWORD' />
+                    <button className='btn-primary' type='submit' style={{display:'inline-block',marginTop:'30px'}}>LOG IN</button>
+                    <medium>Or <a onClick={()=> {
+                      browserHistory.replace('/auth/register')
+                    }}>Register</a> </medium>
+                    <medium style={{marginTop:'67px',marginLeft:'40px'}}><a>download Mobile PRouDS</a></medium>
 
-                    e.preventDefault()
-                  }
-                }>
-                
-                <Input inputName='USERNAME' />
-                <Input inputName='PASSWORD' />
-                <button className='btn-primary' type='submit' style={{display:'inline-block',marginTop:'30px'}}>LOG IN</button>
-                <medium>Or <a onClick={()=> {
-                  browserHistory.replace('/auth/register')
-                }}>Register</a> </medium>
-                <medium style={{marginTop:'67px',marginLeft:'40px'}}><a>download Mobile PRouDS</a></medium>
+                  </form>
 
-              </form>
-
+                </div>
+              </div>
+            </Loader>
             </div>
           </div>
-          </div>
-        </div>
+
       </div>
 
       )
