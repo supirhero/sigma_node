cc883d9394ca8354fd3039c6e21af4011db33118
dhani48 <dhaniforty8@gmail.com>
2017-08-22 01:07:22 +0700

    push

--- src/components/ProjectWorkplan.jsx
+++ src/components/ProjectWorkplan.jsx
@@ -26,32 +26,181 @@ class ProjectWorkplan extends Component {
   }
     componentWillMount() {
       const id =  this.props.state.page.id
-      store.dispatch(getWorkplanView(id))
+      // store.dispatch(getWorkplanView(id))
       store.dispatch(getTaskView(id))
 
 
     }
+
+
     render(){
       const { handleSubmit } = this.props;
 
-      const workplan = this.props.state.data.workplan
-      const workplan_view = this.props.state.data.parent
 
-      const workplan2=[
-        {
+
+      // const workplan = this.props.state.data.workplan
+      const workplan_view = this.props.state.data.parent ? this.props.state.data.parent : null
+      const treeData = {
+          root: {
+              id: -1,
+              'Name': 'Root',
+              children: [
+                  {
+                      id: 1,
+                      parentId: -1,
+                      Name: 'Category 1',
+                      GUID: '8f7152dc-fed7-4a65-afcf-527fceb99865',
+                      Email: 'hgardnero6@ed.gov',
+                      Gender: 'Male',
+                      Address: '605 Manley Park',
+                      'Phone Number': '31-(678)495-4134',
+                      children: [
+                          {
+                              id: 11,
+                              parentId: 1,
+                              Name: 'Category 11',
+                              GUID: '8f7152dc-fed7-4a65-afcf-527fceb991865',
+                              Email: 'hgardneross6@ed.gov',
+                              Gender: 'Male',
+                              Address: '12 Manley Park',
+                              'Phone Number': '31-(678)495-4134',
+                          },
+                          {
+                              id: 12,
+                              parentId: 1,
+                              Name: 'Category 12',
+                              GUID: '8f7152dc-fed7-4acf-527fceb991865',
+                              Email: 'hgardneross6@ed.gov',
+                              Gender: 'Male',
+                              Address: '12 Manley Park',
+                              'Phone Number': '31-(678)495-4134',
+                              children: [
+                                   {
+                                      id: 121,
+                                      parentId: 12,
+                                      Name: 'Category 121',
+                                      GUID: '8f7q2dc-fedsss7-4acf-527fceb991865',
+                                      Email: 'hgoss6@eds.gov',
+                                      Gender: 'Male',
+                                      Address: '21 fake Park',
+                                      'Phone Number': '31-(678)495-4134',
+                                  },
+                                  {
+                                      id: 122,
+                                      parentId: 12,
+                                      Name: 'Category 122',
+                                      GUID: '8f7q2dc-fed7-4acf-527fceb991865',
+                                      Email: 'hgoss6@ed.gov',
+                                      Gender: 'Male',
+                                      Address: '21 fake Park',
+                                      'Phone Number': '31-(678)495-4134',
+                                      children: [
+                                          {
+                                              id: 1221,
+                                              parentId: 122,
+                                              Name: 'Category 1211',
+                                              GUID: '8f7q2dc-facf-527fceb991865',
+                                              Email: 'hgossjdjdjdj6@ed.gov',
+                                              Gender: 'Male',
+                                              Address: '21 fdjdjake Park',
+                                              'Phone Number': '31-(678)495-4134'
+                                          }
+                                      ]
+                                  }
+                              ]
+                          }
+                      ]
+                  },
+                  {
+                      id: 2,
+                      parentId: -1,
+                      Name: 'Category 2',
+                      GUID: '8f7q2dc-facf-527fcebdk=-jdjd991865',
+                      Email: 'hehehe@ed.gov',
+                      Gender: 'Male',
+                      Address: '212 Park',
+                      'Phone Number': '31-(678)495-4134',
+                      children: [
+                          {
+                              id: 21,
+                              parentId: 2,
+                              Name: 'Category 21',
+                              GUID: '8f7q2dc-facf-527fcsw-jdjd991865',
+                              Email: 'hehehe@ed.gov',
+                              Gender: 'Male',
+                              Address: '21112 Park',
+                              'Phone Number': '31-(678)495-4134',
+                              leaf: false
+                          }
+                      ]
+                  }
+              ]
+          }
+      };
+
+      const treeConfig = {
+        stateKey: 'workplan-grid',
+         showTreeRootNode: false,
+        gridType: 'tree', // either `tree` or `grid`,
+        data: treeData,
+        stateful: true,
+        plugins:{
+          BULK_ACTIONS: {
+            enabled: false,
+          }
+        },
+        // showTreeRootNode: false, // dont display root node of tree
+        columns: [
+            {
+                name: 'Name',
+                width: '30%',
+                className: 'additional-class',
+                dataIndex: 'Name',
+                sortable: false,
+                expandable: true
+            },
+            {
+                name: 'Phone Number',
+                dataIndex: 'Phone Number',
+                sortable: false,
+                className: 'additional-class'
+            },
+            {
+                name: 'Email',
+                dataIndex: 'Email',
+                sortable: false,
+                className: 'additional-class',
+                defaultSortDirection: 'descend'
+            },
+            {
+                name: 'Address',
+                dataIndex: 'Address',
+                sortable: false,
+                className: 'additional-class'
+            }
+        ],
+    };
+      const workplan2=
+        {root : {
           task: 'Transaction Based Managed Service 2017',
           work: 258,
+          parentId: null,
+          parentId: -1,
           work_total: 55328,
           duration: 12,
           start_date: '08 Apr 2017',
           end_date: '23 Apr 2017',
           complete: 0.41,
           resources: '2 people',
-          sub: [
+          children: [
             {
+              categoryCode: 'as-ffw-34neh-',
+                id: 1,
+                parentId: -1,
                 task: 'Working Activity',
                 work: 258,
-                work_total: 55328,
+                work_total: 55318,
+                column: 'Column',
                 duration: 12,
                 start_date: '08 Apr 2017',
                 end_date: '23 Apr 2017',
@@ -59,6 +208,10 @@ class ProjectWorkplan extends Component {
                 resources: '2 people',
                 sub: [
                   {
+                    categoryCode: 'as-ffw-34wneh-',
+
+                    id: 12,
+                    parentId: 1,
                     task: 'Annual Working',
                     work: 258,
                     work_total: 55328,
@@ -68,130 +221,14 @@ class ProjectWorkplan extends Component {
                     complete: 0.41,
                     resources: '2 people',
                   },
-                  {
-                    task: 'Overtime',
-                    work: 258,
-                    work_total: 55328,
-                    duration: 12,
-                    start_date: '08 Apr 2017',
-                    end_date: '23 Apr 2017',
-                    complete: 0.41,
-                    resources: '2 people',
-                  }
+
                 ]
               },
-              {
-                    task: 'Non Working Activity',
-                    work: 258,
-                    work_total: 55328,
-                    duration: 12,
-                    start_date: '08 Apr 2017',
-                    end_date: '23 Apr 2017',
-                    complete: 0.41,
-                    resources: '2 people',
-                    sub: [
-                      {
-                        task: 'Annual Leave',
-                        work: 258,
-                        work_total: 55328,
-                        duration: 12,
-                        start_date: '08 Apr 2017',
-                        end_date: '23 Apr 2017',
-                        complete: 0.41,
-                        resources: '2 people',
-                      },
-                      {
-                        task: 'Sick Leave',
-                        work: 258,
-                        work_total: 55328,
-                        duration: 12,
-                        start_date: '08 Apr 2017',
-                        end_date: '23 Apr 2017',
-                        complete: 0.41,
-                        resources: '2 people',
-                      }
-                    ]
-                  }
-            ]
-          },
-          {
-            task: 'Transaction Based Managed Service 2017',
-            work: 258,
-            work_total: 55328,
-            duration: 12,
-            start_date: '08 Apr 2017',
-            end_date: '23 Apr 2017',
-            complete: 0.41,
-            resources: '2 people',
-            sub: [
-              {
-                  task: 'Working Activity',
-                  work: 258,
-                  work_total: 55328,
-                  duration: 12,
-                  start_date: '08 Apr 2017',
-                  end_date: '23 Apr 2017',
-                  complete: 0.41,
-                  resources: '2 people',
-                  sub: [
-                    {
-                      task: 'Annual Working',
-                      work: 258,
-                      work_total: 55328,
-                      duration: 12,
-                      start_date: '08 Apr 2017',
-                      end_date: '23 Apr 2017',
-                      complete: 0.41,
-                      resources: '2 people',
-                    },
-                    {
-                      task: 'Overtime',
-                      work: 258,
-                      work_total: 55328,
-                      duration: 12,
-                      start_date: '08 Apr 2017',
-                      end_date: '23 Apr 2017',
-                      complete: 0.41,
-                      resources: '2 people',
-                    }
-                  ]
-                },
-                {
-                      task: 'Non Working Activity',
-                      work: 258,
-                      work_total: 55328,
-                      duration: 12,
-                      start_date: '08 Apr 2017',
-                      end_date: '23 Apr 2017',
-                      complete: 0.41,
-                      resources: '2 people',
-                      sub: [
-                        {
-                          task: 'Annual Leave',
-                          work: 258,
-                          work_total: 55328,
-                          duration: 12,
-                          start_date: '08 Apr 2017',
-                          end_date: '23 Apr 2017',
-                          complete: 0.41,
-                          resources: '2 people',
-                        },
-                        {
-                          task: 'Sick Leave',
-                          work: 258,
-                          work_total: 55328,
-                          duration: 12,
-                          start_date: '08 Apr 2017',
-                          end_date: '23 Apr 2017',
-                          complete: 0.41,
-                          resources: '2 people',
-                        }
-                      ]
-                    }
-              ]
-            }
 
-          ]
+            ]
+          }
+}
+
       return(
         <div className='project-workplan'>
           <div className='grid wrap'>
@@ -304,61 +341,10 @@ class ProjectWorkplan extends Component {
                 </div> */}
                 <div className='grid wrap'>
                   <div className='unit whole'>
-                    {
-                      !workplan ? <PageLoader/> :
-                      <table className='table workplan'>
-                      <thead>
-                        <Header style={{padding: '20px 0 0 20px'}} text='Project Detail'/>
-
-                        <tr>
-                          <th>TASK</th>
-                          <th>WORK</th>
-                          <th>WORK<br/>TOTAL</th>
-                          <th>DURATION<br/>(DAYS)</th>
-                          <th>START<br/>DATE</th>
-                          <th>END<br/>DATE</th>
-                          <th>% WORK<br/>COMPLETE</th>
-                          <th>RESOURCES<br/></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr onClick={
-                          e => {
-                              if (this.state.clicked) {
-                                this.setState({clicked:false})
-                              }
-                              else {
-                                this.setState({clicked:true})
-                              }
-                              e.preventDefault()
-                            }
-                          }>
-
-                          <td  style={{paddingLeft: '20px'}}><span style={{verticalAlign:'middle', fontSize:'16px', color:'black'}} className='material-icons'>{workplan.children.length!=0 && this.state.clicked ? 'expand_more': 'expand_less'}</span>&nbsp;&nbsp;&nbsp;&nbsp;{workplan.WBS_NAME}</td>
-                          <td>{workplan.WORK}</td>
-                          <td>{workplan.WORK_TOTAL}</td>
-                          <td>{workplan.DURATION}</td>
-                          <td>{workplan.START_DATE}</td>
-                          <td>{workplan.END_DATE}</td>
-                          <td>{workplan.WORK_PERCENT_COMPLETE}</td>
-                          <td>{workplan.LEAF}</td>
-                        </tr>
-                      </tbody>
-                      {
-                        workplan.children.length !=0 && this.state.clicked &&
-
-                        workplan.children.map((value,index) => {
-                          return(
-
-                              <WorkplanRow  key={index} data={value}>
-
-                              </WorkplanRow>
-
-                        )
-                      })
-                    }
-
-                    </table>}
+                      {/* // !workplan ? <PageLoader></PageLoader> : */}
+                      <Grid
+                        {...treeConfig}
+                        ></Grid>
                   </div>
                 </div>
               </div>
